9e7067604eccb8e81d2eee8207d10d8aa8bee119
enbake <enbake@enbake-Aspire-E1-531.(none)>
2015-07-18 12:41:09 +0530

    changes for response

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,19 +5,15 @@ class HomeController < ApplicationController
 
   def after_authantication_create
     #auth = request.env['omniauth.auth']
-    begin
-      response = HTTParty.post("https://login.bigcommerce.com/oauth2/token",
-       {:body => { "client_id" => 'a796d7nwww8fz77h3l4qhp7axsn8bpe',
-       "client_secret" => '7ahfar8emr3apilvvx2j1kehbsnksng',
-       "code" => params[:code], "scope" => params[:scope],
-       "grant_type" => "authorization_code", "redirect_uri" => "https://bigcommerce.pagekite.me/auth/bigcommerce/callback",
-       "context" => params[:context]}.to_json,
-       :headers => { 'Content-Type' => 'application/json' }
-       })
-     rescue
-       flash[:error] = '#{params}'
-     end
-   redirect_to root_path
+    response = HTTParty.post("https://login.bigcommerce.com/oauth2/token",
+     {:body => { "client_id" => 'a796d7nwww8fz77h3l4qhp7axsn8bpe',
+     "client_secret" => '7ahfar8emr3apilvvx2j1kehbsnksng',
+     "code" => params[:code], "scope" => params[:scope],
+     "grant_type" => "authorization_code", "redirect_uri" => "https://bigcommerce.pagekite.me/auth/bigcommerce/callback",
+     "context" => params[:context]}.to_json,
+     :headers => { 'Content-Type' => 'application/json' }
+     })
+     flash[:error] = "#{response.body} ----- #{params}"
   end
 
 end
